fddc52eb89eeedf66f3bbf866a95db3f103cb7ee
david landau <thelandaus@gmail.com>
2018-05-07 03:30:09 +0000

    automatically backed up by learn

--- countdown.rb
+++ countdown.rb
@@ -1,6 +1,9 @@
 #write your code here
 
-def countdown(int)
-  while int > 0
-    
+def countdown(number)
+  while number > 0
+    puts "#{number} SECOND(S)!"
+    number -= 1
+  end
+  return 
 end
