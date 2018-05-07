c7f7a68bdcb0a0bf4f470c8fddb57f1b20c0b37e
david landau <thelandaus@gmail.com>
2018-05-07 03:36:07 +0000

    automatically backed up by learn

--- countdown.rb
+++ countdown.rb
@@ -4,8 +4,15 @@ def countdown(number)
   while number > 0
     puts "#{number} SECOND(S)!"
     number -= 1
-    sleep 2
   end
   return "HAPPY NEW YEAR!"
 end
 
+def countdown_with_sleep(number)
+  while number > 0
+    puts "#{number} SECOND(S)!"
+    number -= 1
+    sleep 2
+  end
+  return "HAPPY NEW YEAR!"
+end
